150025bd092ed6ff44780ef9d8f037c02e136e0e
Archianne <helena19w@gmail.com>
2021-05-22 15:18:05 +0100

    innerHTML

--- leaderboard.js
+++ leaderboard.js
@@ -1,20 +1,37 @@
 const inputUsername = document.querySelector("#addUsername");
 const enterButton = document.querySelector("#enterButton");
-const TOKEN = "60a8e2c18f40bb64ec94690b";
-const PRIVATE = "3XrM-f9kdUeAuuv95QrE8Aal7MEJy5UUGMfgoBYZMR4A"
 const displayScore = document.querySelector(".displayScore");
+let results = "";
+
+const TOKEN = "60a8e2c18f40bb64ec94690b";
+const PRIVATE = "3XrM-f9kdUeAuuv95QrE8Aal7MEJy5UUGMfgoBYZMR4A";
+
+async function addPlayer() {
+    displayScore.innerHTML = "";
+  const addURL = `http://dreamlo.com/lb/${PRIVATE}/add/${inputUsername.value}/1000/`;
+  await fetch(addURL);
+  keepScore();
+}
 
 async function keepScore() {
   const URL = `http://dreamlo.com/lb/${TOKEN}/json`;
   const response = await fetch(URL);
   const data = await response.json();
   console.log(data);
-}
+  showLeaderboard();
 
-keepScore();
-
-
-async function addPlayer() {
-    const addURL = `http://dreamlo.com/lb/${PRIVATE}/add/${inputUsername.value}/1000/`
-    await fetch(addURL);
+ function showLeaderboard() {
+    for (let i = 0; i < 10; i++) {
+      results = data.dreamlo.leaderboard.entry[i];
+      displayScore.innerHTML += `
+        <table>
+        <tr>
+        <td>${results.name}</td>
+        <td>${results.score}</td>
+        <td>${results.date}</td>
+        </tr>
+        </table>
+        `;
+    }
+  }
 }
