966b86fc59dff66600d624c3cf02e24640efcc78
Archianne <helena19w@gmail.com>
2021-05-22 11:13:35 +0100

    countdown

---
+++ countdown.js
@@ -0,0 +1,21 @@
+let countdown = document.querySelector("#countdown");
+let timeRemaining = 60;
+
+//start count
+function startCountdown() {
+  action = setInterval(function () {
+    timeRemaining -= 1;
+
+    countdown.innerHTML = timeRemaining;
+    if (timeRemaining == 0) {
+      stopCountdown();
+    }
+  }), 1000;
+}
+
+//stop count
+function stopCountdown() {
+  clearInterval(action);
+}
+
+startCountdown()
