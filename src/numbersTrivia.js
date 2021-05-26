0da9d14857b20683fd5acd96ddbf66e22ffff840
Archianne <helena19w@gmail.com>
2021-05-26 18:51:41 +0100

    .

--- src/numbersTrivia.js
+++ src/numbersTrivia.js
@@ -4,10 +4,10 @@ const numbersapi = document.querySelector(".numbersapi");
 async function getTriviaFact() {
     randomNumberForTrivia = Math.floor(Math.random() * 100) + 1;
   let response = await fetch(
-    `http://numbersapi.com/${randomNumberForTrivia}/trivia`
+    `https://numbersapi.com/${randomNumberForTrivia}/trivia`
   );
   let data = await response.text();
-
+    console.log(data);
   numbersapi.innerHTML = `
   <p id="numberTrivia">${data}</p>
   <button class="numbersButtonRefresh" onclick="getTriviaFact()"><i class="fas fa-sync"></i></button>`
